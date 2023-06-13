01691bdab9f6592d8e606fe464867d56de5d4ed2
Dru Banks <132113497+S0KRAT3Z@users.noreply.github.com>
2023-06-13 09:11:12 -0500

    Replaced deprecated key server functions.
    There was a long-standing issue with the code using apt-key to import Kali repo keys. The update fixes the problem by downloading the keys directly from the source using Wget and piping the output into modern GPG commands.

--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-source "conf.sh";
+source "conf.sh"
 
-PROGRAM_PREFIX="";
+PROGRAM_PREFIX=""
 
-# If the installation directory is not in PATH issue a warning:
-if ! echo $PATH | grep -q "$DIR";
-then
-    echo "Warning: '$DIR' is not in your PATH.";
-    echo "         To use this program add '$DIR'";
-    echo "         to your PATH or manually copy";
-    echo "         katoolin3.py somewhere.";
-    echo;
-    PROGRAM_PREFIX="$DIR/";
+# If the installation directory is not in PATH, issue a warning
+if ! echo $PATH | grep -q "$DIR"; then
+    echo "Warning: '$DIR' is not in your PATH."
+    echo "         To use this program, add '$DIR' to your PATH or manually copy katoolin3.py somewhere."
+    echo
+    PROGRAM_PREFIX="$DIR/"
 fi
 
-# Check if shebang from katoolin3.py can execute:
-/usr/bin/env python3 -V >/dev/null || die "Please install 'python3'";
+# Check if the shebang from katoolin3.py can execute
+/usr/bin/env python3 -V >/dev/null || { echo "Please install 'python3'"; exit 1; }
 
-# Install all dependencies:
-apt-key adv -qq --keyserver pool.sks-keyservers.net --recv-keys ED444FF07D8D0BF6 || apt-key adv -qq --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys ED444FF07D8D0BF6 || die "This may be a server issue. Please try again later";
-apt-get -qq -y -m install python3-apt || die;
+# Import the GPG key for the Kali Linux repository
+wget -q -O - https://archive.kali.org/archive-key.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/kali-archive-keyring.gpg || { echo "Failed to import the GPG key"; exit 1; }
 
-install -T -g root -o root -m 555 ./katoolin3.py "$DIR/$PROGRAM" || die;
+# Install the required dependencies
+apt-get -qq -y -m install python3-apt || { echo "Failed to install dependencies"; exit 1; }
+
+# Install katoolin3.py
+install -T -g root -o root -m 555 ./katoolin3.py "$DIR/$PROGRAM" || { echo "Failed to install katoolin3.py"; exit 1; }
 
 echo "Successfully installed."
-echo "Run it with 'sudo $PROGRAM_PREFIX$PROGRAM'.";
-exit 0;
+echo "Run it with 'sudo $PROGRAM_PREFIX$PROGRAM'."
+exit 0
